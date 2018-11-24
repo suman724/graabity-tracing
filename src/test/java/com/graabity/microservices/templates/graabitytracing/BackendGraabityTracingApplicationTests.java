abf906bcc9b98acc4d9bdc19ba192c3fe0609947
Suman Addanki <suman724@gmail.com>
2018-11-24 14:43:31 -0500

    Fixed the tests

--- src/test/java/com/graabity/microservices/templates/graabitytracing/BackendGraabityTracingApplicationTests.java
+++ src/test/java/com/graabity/microservices/templates/graabitytracing/BackendGraabityTracingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = FrontendGraabityTracingApplication.class)
-public class FrontendGraabityTracingApplicationTests {
+@SpringBootTest(classes = BackendGraabityTracingApplication.class)
+public class BackendGraabityTracingApplicationTests {
 
 	@Test
 	public void contextLoads() {
